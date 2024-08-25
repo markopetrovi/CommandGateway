4562019a6ae31a3ab70d5ed5aebadbb842658e9c
Marko Petrović <petrovicmarko2006@gmail.com>
2024-08-26 01:35:30 +0200

    Add open_socket to client.c

--- src/client/client.c
+++ src/client/client.c
@@ -2,6 +2,17 @@
 
 int sockfd;
 
+void open_socket()
+{
+    struct sockaddr_un sock = { 0 };
+	
+	fill_sockaddr(&sock);
+    check( sockfd=socket(AF_UNIX, SOCK_SEQPACKET | SOCK_CLOEXEC, 0) )
+    lprintf("[DEBUG]: Connecting to socket...\n");
+    check( connect(sockfd, &sock, sizeof(struct sockaddr_un)) )
+    lprintf("[DEBUG]: Connected.\n");
+}
+
 int main(int argc, char *argv[])
 {
     init_program();
