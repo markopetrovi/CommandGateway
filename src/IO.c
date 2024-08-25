4c57df1ff6b3fa0df04f5ee86a05de24d2605606
Marko Petrović <petrovicmarko2006@gmail.com>
2024-08-25 23:45:06 +0200

    Move IO.c and request_dispatcher.c to src

--- src/IO.c
+++ src/IO.c
@@ -6,7 +6,7 @@ ssize_t lread(int fd, void *buf, size_t count)
 
 	if (ret < 0) {
 		if (errno == EWOULDBLOCK || errno == EAGAIN)
-			report_error_and_die("Client taking too long to respond.\n");
+			report_error_and_die("Socket taking too long to respond.\n");
 		dlperror("read");
 		report_error_and_die("Failed to read the request.\n");
 	}
@@ -20,7 +20,7 @@ ssize_t lwrite(int fd, const void *buf, size_t count)
 	
 	if (ret < 0) {
 		if (errno == EWOULDBLOCK || errno == EAGAIN)
-			report_error_and_die("Client taking too long to respond.\n");
+			report_error_and_die("Socket taking too long to respond.\n");
 		dlperror("write");
 		report_error_and_die("Failed to send the response.\n");
 	}
